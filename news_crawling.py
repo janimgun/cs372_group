c141b115734ad3176d0a69885efed3fcf8661a53
janimgun <kjk6729@gmail.com>
2020-06-20 00:52:10 +0900

    news_crawling until max_date

--- news_crawling.py
+++ news_crawling.py
@@ -5,22 +5,19 @@ import requests
 import re
 import pandas as pd
 import os
-from datetime import datetime
 import datetime
 
 os.chdir('C:\cs372_group')  # 디렉토리 주소
 
 df_news = pd.DataFrame()
 
-
-def crawler(company_code, date):
+def crawler(company_code, date_max):
     global df_news
     page = 1
 
     while 1:
-
+        df_result = pd.DataFrame()
         url = 'https://finance.naver.com/item/news_news.nhn?code=' + str(company_code) + '&page=' + str(page)
-        # print(url)
         source_code = requests.get(url).text
         html = BeautifulSoup(source_code, "lxml")
 
@@ -43,40 +40,47 @@ def crawler(company_code, date):
         # 뉴스 날짜
         dates = html.select('.date')
         date_result = [date.get_text() for date in dates]
-        # print(date_result)
         date_day = []
         date_time = []
         for i in date_result:
-            temp = i.lstrip().split(' ')
-            date_day.append(temp[0])
-            date_time.append(int(temp[1].split(':')[0]))
+            date_day.append(i.lstrip().split(' ')[0])
+            date_time.append(int(i.lstrip().split(' ')[1].split(':')[0]))
 
         # 뉴스 매체
         sources = html.select('.info')
         source_result = [source.get_text() for source in sources]
+
+        # 변수들 합쳐서 해당 디렉토리에 csv파일로 저장하기
+
+        text_result = []
+        for url_link in link_result:
+            source_code2 = requests.get(url_link).text
+            html2 = BeautifulSoup(source_code2, 'lxml')
+            texts = html2.select('.scr01')
+            tt = [text.get_text() for text in texts]
+            ttt = re.sub('\n|\t|\'', '', tt[0])
+            ttt = re.split('ⓒ|▶', ttt)
+            # print(ttt)
+            text_result.append(ttt[0])
+
         company_result = [str(company_code)] * len(date_result)
 
-        result = {"코드": company_result, "날짜": date_day, "시간": date_time, "언론사": source_result, "기사제목": title_result,
-                  "링크": link_result}
+        result = {"코드": company_result, "날짜": date_day,"시간": date_time, "언론사": source_result, "기사제목": title_result,
+                  "링크": link_result, "기사내용": text_result}
         df_result = pd.DataFrame(result)
+        df_result = df_result.set_index("코드")
 
-        # 해당 날짜와 기사를 올린 날짜가 일치하는 것만 가져오기, 여기가 날짜에 대한 조건문
-        # 조건 1, 해당 날짜만 가져오기
-        # df_result = df_result.loc[(df_result["날짜"] == date)]
-        
-        # 조건 2, 그 전날 장이 끝난 4시 이후부터 그 당일 장이 시작하기 전 9시 까지의 뉴스
-        #YYYY.MM.DD로 나와있는 date를 변환 후
-        date_datetime = datetime.datetime.strptime(date, '%Y.%m.%d')
+        date_datetime = datetime.datetime.strptime(date_max, '%Y.%m.%d') # max datetime
         date_before = date_datetime + datetime.timedelta(days=-1)
         date_before = date_before.strftime("%Y.%m.%d")
-        # df_result = df_result.loc[(df_result["날짜"] == )]
-        df_result = df_result.loc[((df_result["날짜"] == date_before) & (df_result["시간"] > 15)) ^ ((df_result["날짜"] == date) & (df_result["시간"] < 9))]
-
+        pick_index = []
+        for i in range(len(df_result.index)):
+            if datetime.datetime.strptime(df_result.iloc[i]["날짜"], '%Y.%m.%d') > date_datetime:
+                pick_index.append(i)
+        df_result = df_result.iloc[pick_index,:]
+        print(df_result)
         if df_result.empty:
-            if not df_news.empty:
-                break
-            page += 1
-            continue
+            break
         else:
             link_result = []
             for i in range(len(df_result.index)):
@@ -95,9 +99,9 @@ def crawler(company_code, date):
             df_result["기사 내용"] = text_result
             page += 1
             df_news = pd.concat([df_news, df_result])
+    df_news.to_csv('news_crawling.csv', mode='w', encoding='utf-8-sig')
+    return df_news
 
-    print(df_news)
-    df_news.to_csv('company_news.csv', mode='w', encoding='utf-8-sig')
     # 종목 리스트 파일 열기
 
 
@@ -132,9 +136,7 @@ def get_price(company_code, date):
     print(df_inf)
 
 
-# 회사명을 종목코드로 변환
-
-def convert_to_code(company, date):
+def convert_to_code(company, maxpage):
     data = pd.read_csv('company_list.txt', dtype=str, sep='\t')  # 종목코드 추출
     company_name = data['회사명']
     keys = [i for i in company_name]  # 데이터프레임에서 리스트로 바꾸기
@@ -147,35 +149,25 @@ def convert_to_code(company, date):
     pattern = '[0-9a-zA-Z가-힣]+'
     # if bool(re.match(pattern, company)) == True:  # Input에 이름으로 넣었을 때
     if bool(str(company) in keys) == True:
-        print("in name")
         company_code = dict_result.get(str(company))
-        crawler(company_code, date)
-        get_price(company_code, date)
+        crawler(company_code, maxpage)
 
 
     elif bool(str(company) in values) == True:  # Input에 종목코드로 넣었을 때
-        print("in code")
         company_code = str(company)
-        crawler(company_code, date)
-        get_price(company_code, date)
+        crawler(company_code, maxpage)
+
     else:
-        print("oh no")
+        print("no name or code")
 
 
 def main():
-    """
-    company에 company_list에 있는 회사 중 하나를 적고,
-    max_date에 날짜를 적으면,
-    현재부터 날짜까지 그 회사의 뉴스를 네이버 금융에서 크롤링하여 news_crawling.csv에 저장한다.
-    :return:
-    """
-    company = "005930"  # 종목 코드, 005930은 삼성전자
-    crawling_date = "2020.06.18"  # 뉴스를 가져올 날짜, 연도.월.일 로 적으면 된다.
+    # info_main = input("=" * 50 + "\n" + "실시간 뉴스기사 다운받기." + "\n" + " 시작하시려면 Enter를 눌러주세요." + "\n" + "=" * 50)
 
-    if crawling_date == datetime.datetime.today().strftime("%Y.%m.%d"):
-        print("It's today, can't get result")
-    else:
-        convert_to_code(company, crawling_date)
+    company = "삼성전자"
+    max_date = "2020.06.15"
+
+    convert_to_code(company, max_date)
 
 
 main()
